418d1aab7b62cf87f86ed706fcac70de3d9afb29
jamesdanielwhitford <james.whitford0@gmail.com>
2025-06-02 16:39:57 +0200

    added handler for slash command

--- index.js
+++ index.js
@@ -1,7 +1,38 @@
 const express = require('express');
 const superagent = require('superagent');
+const bodyParser = require("body-parser");
 
 const app = express();
+
+app.use(bodyParser.urlencoded());
+
+app.post('/slack/command/stats', [function(req,res){
+  const slackReqObj = req.body;
+  const packageJson = require('./package.json');
+
+  const current_time = new Date(); 
+  const stats = {
+    name: packageJson.name,
+    version: packageJson.version,
+    environment: process.env.NODE_ENV,
+    platform: process.platform,
+    architecture: process.arch,
+    node_version: process.version,
+    pid: process.pid,
+    current_server_time: current_time.toString(),
+    uptime: process.uptime(),
+    memory_usage: process.memoryUsage()
+  };
+
+  const response = {
+    response_type: 'in_channel',
+    channel: slackReqObj.channel_id,
+    text: JSON.stringify(stats, null, '\t')
+  };
+
+  return res.json(response); 
+}]);
+
 
 let port = process.env.PORT || 3000;
 app.listen(port, ()=>{
